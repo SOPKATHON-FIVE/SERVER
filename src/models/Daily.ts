1084546b466c8c9e2f4c3e5eb0463f694e2b0dba
namjiyun <namjiyun3275@gmail.com>
2022-05-22 01:50:13 +0900

    [Feat] daily model 구현

--- src/models/Daily.ts
+++ src/models/Daily.ts
@@ -1,24 +1,30 @@
-import mongoose from "mongoose";
-import { DailyInfo } from "../interfaces/daily/DailyInfo";
+import mongoose from 'mongoose';
+import { DailyInfo } from '../interfaces/daily/DailyInfo';
 
 const DailySchema = new mongoose.Schema({
-    dailyName: {
+  dailyName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Number,
+    required: true,
+  },
+  userName: {
+    type: String,
+  },
+  missions: [
+    {
+      mission: {
         type: String,
         required: true,
-
+      },
     },
-    userId: {
-        type: Number,
-        required: true,
-    },
-    missions: [
-        {
-            mission: {
-                type: String,
-                required: true
-            }
-        }
-    ]
+  ],
 });
 
-export default mongoose.model<DailyInfo & mongoose.Document>("Daily", DailySchema);
+export default mongoose.model<DailyInfo & mongoose.Document>('Daily', DailySchema);
