1016db6d5477f423773208d81031bb15ed94a050
namjiyun <namjiyun3275@gmail.com>
2022-05-22 04:37:11 +0900

    [Fix] description 삭제

--- src/routes/DailyRouter.ts
+++ src/routes/DailyRouter.ts
@@ -1,22 +1,13 @@
-import { Router } from "express";
-import { DailyController } from "../controllers";
-const { body } = require("express-validator/check");
+import { Router } from 'express';
+import { DailyController } from '../controllers';
+const { body } = require('express-validator/check');
 
 import UserMissionController from '../controllers/UserMissionController';
 const router: Router = Router();
 
-router.post('/my', [
-    body("dailyName").notEmpty(),
-    body("description").notEmpty(),
-    body("userId").notEmpty(),
-    body("userName").notEmpty(),
-    body("missions").notEmpty()
-
-], DailyController.createDaily);
-
+router.post('/my', [body('dailyName').notEmpty(), body('userId').notEmpty(), body('userName').notEmpty(), body('missions').notEmpty()], DailyController.createDaily);
 
 router.get('/my/:userId', DailyController.getDaily);
-
 
 router.get('/', DailyController.getAllDaily);
 router.post('/', UserMissionController.addUserDaily);
