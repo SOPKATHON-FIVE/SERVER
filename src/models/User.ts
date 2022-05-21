d10c8dad16e33c08c36c77d852961a0e8e35a95c
namjiyun <namjiyun3275@gmail.com>
2022-05-22 01:50:32 +0900

    [Feat] 유저 mission model 구현

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,27 +1,26 @@
-import mongoose from "mongoose";
-import { UserMissionInfo } from "../interfaces/UserMissionInfo";
-
+import mongoose from 'mongoose';
+import { UserMissionInfo } from '../interfaces/userMission/UserMissionInfo';
 
 const UserMissionSchema = new mongoose.Schema({
-    userId: {
-        type: Number,
-        required: true,
+  userId: {
+    type: Number,
+    required: true,
+  },
+  userName: {
+    type: String,
+  },
+  dailyId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'Daily',
+  },
+  completeMissions: [
+    {
+      mission: {
+        type: String,
+      },
     },
-    dailyId: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: "Daily"
-    },
-    completeMissions: [
-        {
-            mission: {
-                type: String
-            },
-            check: {
-                type: Boolean
-            }
-        }
-    ],
+  ],
 });
 
-export default mongoose.model<UserMissionInfo & mongoose.Document>("UserMission", UserMissionSchema);
+export default mongoose.model<UserMissionInfo & mongoose.Document>('UserMission', UserMissionSchema);
