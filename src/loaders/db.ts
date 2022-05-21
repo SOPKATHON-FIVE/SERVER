24530bb4f511bcb2a4d037562257e2438a20962a
namjiyun <namjiyun3275@gmail.com>
2022-05-22 01:52:26 +0900

    [Feat] 컬렉션 생성 코드 추가

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,17 +1,26 @@
-import mongoose from "mongoose";
-import config from "../config";
+import mongoose from 'mongoose';
+import config from '../config';
+import User from '../models/User';
+import Daily from '../models/Daily';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(config.mongoURI);
+  try {
+    await mongoose.connect(config.mongoURI);
 
-        mongoose.set('autoCreate', true);
+    mongoose.set('autoCreate', true);
 
-        console.log("Mongoose Connected ...");
-    } catch (err: any) {
-        console.error(err.message);
-        process.exit(1);
-    }
+    console.log('Mongoose Connected ...');
+
+    User.createCollection().then(function (collection) {
+      console.log('movie collection created');
+    });
+    Daily.createCollection().then(function (collection) {
+      console.log('review collection created');
+    });
+  } catch (err: any) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
